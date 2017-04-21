01b1e5561b884ae578dcd68d5fc08c0485b79141
lmz <limuzi@8dol.com>
2017-04-21 14:05:15 +0800

    BMI计算器

---
+++ src/page/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id='index'>
+    <div class="nav">
+      <div class="title">免费的在线BMI计算器</div>
+    </div>
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+<style lang="scss">
+  #index{
+    .nav{
+      width:100%;
+      height: 4.5rem;
+      background: #b9e6fa;
+    }
+  }
+
+</style>
